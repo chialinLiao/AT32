cdc0df2b42ea1595ffb2b19d76e064c4d31c0349
Chialin Liao <chialin.Liao@apexmedicalcrop.com>
2022-05-05 09:41:04 +0800

    [feat] implement the usbd cdc mode

--- user/src/main.c
+++ user/src/main.c
@@ -26,79 +26,26 @@
 
 #include "at32f435_437_board.h"
 #include "at32f435_437_clock.h"
+#include "usb_conf.h"
+#include "usb_core.h"
+#include "usbd_int.h"
+#include "cdc_class.h"
+#include "cdc_desc.h"
 
-/** @addtogroup AT32F435_periph_template
+/** @addtogroup AT32F435_periph_examples
   * @{
   */
 
-/** @addtogroup 435_LED_toggle LED_toggle
+/** @addtogroup 435_USB_device_vcp_loopback USB_device_vcp_loopback
   * @{
   */
 
-#define DELAY                            100
-#define FAST                             1
-#define SLOW                             4
-
-uint8_t g_speed = FAST;
-
-void button_exint_init(void);
-void button_isr(void);
-
-/**
-  * @brief  configure button exint
-  * @param  none
-  * @retval none
-  */
-void button_exint_init(void)
-{
-  exint_init_type exint_init_struct;
-
-  crm_periph_clock_enable(CRM_SCFG_PERIPH_CLOCK, TRUE);
-  scfg_exint_line_config(SCFG_PORT_SOURCE_GPIOA, SCFG_PINS_SOURCE0);
-
-  exint_default_para_init(&exint_init_struct);
-  exint_init_struct.line_enable = TRUE;
-  exint_init_struct.line_mode = EXINT_LINE_INTERRUPUT;
-  exint_init_struct.line_select = EXINT_LINE_0;
-  exint_init_struct.line_polarity = EXINT_TRIGGER_RISING_EDGE;
-  exint_init(&exint_init_struct);
-
-  nvic_priority_group_config(NVIC_PRIORITY_GROUP_4);
-  nvic_irq_enable(EXINT0_IRQn, 0, 0);
-}
-
-/**
-  * @brief  button handler function
-  * @param  none
-  * @retval none
-  */
-void button_isr(void)
-{
-  /* delay 5ms */
-  delay_ms(5);
-
-  /* clear interrupt pending bit */
-  exint_flag_clear(EXINT_LINE_0);
-
-  /* check input pin state */
-  if(SET == gpio_input_data_bit_read(USER_BUTTON_PORT, USER_BUTTON_PIN))
-  {
-    if(g_speed == SLOW)
-      g_speed = FAST;
-    else
-      g_speed = SLOW;
-  }
-}
-
-/**
-  * @brief  exint0 interrupt handler
-  * @param  none
-  * @retval none
-  */
-void EXINT0_IRQHandler(void)
-{
-  button_isr();
-}
+/* usb global struct define */
+otg_core_type otg_core_struct;
+uint8_t usb_buffer[256];
+void usb_clock48m_select(usb_clk48_s clk_s);
+void usb_gpio_config(void);
+void usb_low_power_wakeup_config(void);
 
 /**
   * @brief  main function.
@@ -107,23 +54,267 @@ void EXINT0_IRQHandler(void)
   */
 int main(void)
 {
+  uint16_t data_len;
+
+  uint32_t timeout;
+
+  uint8_t send_zero_packet = 0;
+
+  nvic_priority_group_config(NVIC_PRIORITY_GROUP_4);
+
   system_clock_config();
 
   at32_board_init();
 
-  button_exint_init();
+  /* usb gpio config */
+  usb_gpio_config();
 
-  SysTick_Config(1000);
+#ifdef USB_LOW_POWER_WAKUP
+  usb_low_power_wakeup_config();
+#endif
 
+  /* enable otgfs clock */
+  crm_periph_clock_enable(OTG_CLOCK, TRUE);
+
+  /* select usb 48m clcok source */
+  usb_clock48m_select(USB_CLK_HEXT);
+
+  /* enable otgfs irq */
+  nvic_irq_enable(OTG_IRQ, 0, 0);
+
+  /* init usb */
+  usbd_init(&otg_core_struct,
+            USB_FULL_SPEED_CORE_ID,
+            USB_ID,
+            &class_handler,
+            &desc_handler);
   while(1)
   {
-    at32_led_toggle(LED2);
-    delay_ms(g_speed * DELAY);
-    at32_led_toggle(LED3);
-    delay_ms(g_speed * DELAY);
-    at32_led_toggle(LED4);
-    delay_ms(g_speed * DELAY);
+    /* get usb vcp receive data */
+    data_len = usb_vcp_get_rxdata(&otg_core_struct.dev, usb_buffer);
+
+    if(data_len > 0 || send_zero_packet == 1)
+    {
+
+      /* bulk transfer is complete when the endpoint does one of the following
+         1 has transferred exactly the amount of data expected
+         2 transfers a packet with a payload size less than wMaxPacketSize or transfers a zero-length packet
+      */
+      if(data_len > 0)
+        send_zero_packet = 1;
+
+      if(data_len == 0)
+        send_zero_packet = 0;
+
+      timeout = 50000;
+      do
+      {
+        /* send data to host */
+        if(usb_vcp_send_data(&otg_core_struct.dev, usb_buffer, data_len) == SUCCESS)
+        {
+          break;
+        }
+      }while(timeout --);
+    }
   }
+}
+
+/**
+  * @brief  usb 48M clock select
+  * @param  clk_s:USB_CLK_HICK, USB_CLK_HEXT
+  * @retval none
+  */
+void usb_clock48m_select(usb_clk48_s clk_s)
+{
+  if(clk_s == USB_CLK_HICK)
+  {
+    crm_usb_clock_source_select(CRM_USB_CLOCK_SOURCE_HICK);
+
+    /* enable the acc calibration ready interrupt */
+    crm_periph_clock_enable(CRM_ACC_PERIPH_CLOCK, TRUE);
+
+    /* update the c1\c2\c3 value */
+    acc_write_c1(7980);
+    acc_write_c2(8000);
+    acc_write_c3(8020);
+#if (USB_ID == 0)
+    acc_sof_select(ACC_SOF_OTG1);
+#else
+    acc_sof_select(ACC_SOF_OTG2);
+#endif
+    /* open acc calibration */
+    acc_calibration_mode_enable(ACC_CAL_HICKTRIM, TRUE);
+  }
+  else
+  {
+    switch(system_core_clock)
+    {
+      /* 48MHz */
+      case 48000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_1);
+        break;
+
+      /* 72MHz */
+      case 72000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_1_5);
+        break;
+
+      /* 96MHz */
+      case 96000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_2);
+        break;
+
+      /* 120MHz */
+      case 120000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_2_5);
+        break;
+
+      /* 144MHz */
+      case 144000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_3);
+        break;
+
+      /* 168MHz */
+      case 168000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_3_5);
+        break;
+
+      /* 192MHz */
+      case 192000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_4);
+        break;
+
+      /* 216MHz */
+      case 216000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_4_5);
+        break;
+
+      /* 240MHz */
+      case 240000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_5);
+        break;
+
+      /* 264MHz */
+      case 264000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_5_5);
+        break;
+
+      /* 288MHz */
+      case 288000000:
+        crm_usb_clock_div_set(CRM_USB_DIV_6);
+        break;
+
+      default:
+        break;
+
+    }
+  }
+}
+
+/**
+  * @brief  this function config gpio.
+  * @param  none
+  * @retval none
+  */
+void usb_gpio_config(void)
+{
+  gpio_init_type gpio_init_struct;
+
+  crm_periph_clock_enable(OTG_PIN_GPIO_CLOCK, TRUE);
+  gpio_default_para_init(&gpio_init_struct);
+
+  gpio_init_struct.gpio_drive_strength = GPIO_DRIVE_STRENGTH_STRONGER;
+  gpio_init_struct.gpio_out_type  = GPIO_OUTPUT_PUSH_PULL;
+  gpio_init_struct.gpio_mode = GPIO_MODE_MUX;
+  gpio_init_struct.gpio_pull = GPIO_PULL_NONE;
+
+  /* dp and dm */
+  gpio_init_struct.gpio_pins = OTG_PIN_DP | OTG_PIN_DM;
+  gpio_init(OTG_PIN_GPIO, &gpio_init_struct);
+
+  gpio_pin_mux_config(OTG_PIN_GPIO, OTG_PIN_DP_SOURCE, OTG_PIN_MUX);
+  gpio_pin_mux_config(OTG_PIN_GPIO, OTG_PIN_DM_SOURCE, OTG_PIN_MUX);
+
+#ifdef USB_SOF_OUTPUT_ENABLE
+  crm_periph_clock_enable(OTG_PIN_SOF_GPIO_CLOCK, TRUE);
+  gpio_init_struct.gpio_pins = OTG_PIN_SOF;
+  gpio_init(OTG_PIN_SOF_GPIO, &gpio_init_struct);
+  gpio_pin_mux_config(OTG_PIN_GPIO, OTG_PIN_SOF_SOURCE, OTG_PIN_MUX);
+#endif
+
+  /* otgfs use vbus pin */
+#ifndef USB_VBUS_IGNORE
+  gpio_init_struct.gpio_pins = OTG_PIN_VBUS;
+  gpio_init_struct.gpio_pull = GPIO_PULL_DOWN;
+  gpio_pin_mux_config(OTG_PIN_GPIO, OTG_PIN_VBUS_SOURCE, OTG_PIN_MUX);
+  gpio_init(OTG_PIN_GPIO, &gpio_init_struct);
+#endif
+
+
+}
+#ifdef USB_LOW_POWER_WAKUP
+/**
+  * @brief  usb low power wakeup interrupt config
+  * @param  none
+  * @retval none
+  */
+void usb_low_power_wakeup_config(void)
+{
+  exint_init_type exint_init_struct;
+
+  crm_periph_clock_enable(CRM_SCFG_PERIPH_CLOCK, TRUE);
+  exint_default_para_init(&exint_init_struct);
+
+  exint_init_struct.line_enable = TRUE;
+  exint_init_struct.line_mode = EXINT_LINE_INTERRUPUT;
+  exint_init_struct.line_select = OTG_WKUP_EXINT_LINE;
+  exint_init_struct.line_polarity = EXINT_TRIGGER_RISING_EDGE;
+  exint_init(&exint_init_struct);
+
+  nvic_irq_enable(OTG_WKUP_IRQ, 0, 0);
+}
+
+/**
+  * @brief  this function handles otgfs wakup interrupt.
+  * @param  none
+  * @retval none
+  */
+void OTG_WKUP_HANDLER(void)
+{
+  exint_flag_clear(OTG_WKUP_EXINT_LINE);
+}
+
+#endif
+
+/**
+  * @brief  this function handles otgfs interrupt.
+  * @param  none
+  * @retval none
+  */
+void OTG_IRQ_HANDLER(void)
+{
+  usbd_irq_handler(&otg_core_struct);
+}
+
+/**
+  * @brief  usb delay millisecond function.
+  * @param  ms: number of millisecond delay
+  * @retval none
+  */
+void usb_delay_ms(uint32_t ms)
+{
+  /* user can define self delay function */
+  delay_ms(ms);
+}
+
+/**
+  * @brief  usb delay microsecond function.
+  * @param  us: number of microsecond delay
+  * @retval none
+  */
+void usb_delay_us(uint32_t us)
+{
+  delay_us(us);
 }
 
 /**
